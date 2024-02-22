1c34866d31f08e4cd1b1ca4e28a1352aa4d735da
LiamTodd <toddliam26@gmail.com>
2024-02-22 17:06:40 +1100

    Integrate custom UI with chart js.

--- static/dashboard/src/App.js
+++ static/dashboard/src/App.js
@@ -3,6 +3,53 @@ import { invoke } from '@forge/bridge';
 import { BULK_PREDICT_FUNCTION_KEY } from './constants';
 import { getAllIssues } from './utils/requestJiraUtils';
 import { constructBulkPayload } from './utils/misc';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => Math.floor(Math.random() * (0 - 100))),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => Math.floor(Math.random() * (0 - 100))),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 function App() {
   useEffect(() => {
@@ -13,7 +60,7 @@ function App() {
     });
   }, []);
 
-  return <div>Hello from custom UI</div>;
+  return <Bar options={options} data={data} />;
 }
 
 export default App;
